0758067bcf69f5e5f49cf95c33855624ec338b15
mukiliderrick <derrickmukili64@gmail.com>
2023-02-12 14:04:10 +0300

    second commit

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+"""
+Contains class BaseModel
+"""
+
 import models
 import uuid
 from datetime import datetime
 
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
 class BaseModel:
+    """The BaseModel class from which future classes will be derived"""
     def __init__(self, *args, **kwargs):
+        """Initialization of the base model"""
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -18,15 +26,22 @@ class BaseModel:
             models.storage.new(self)
 
     def __str__(self):
+        """String representation of the BaseModel class"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """updates the attribute 'updated_at' with the current datetime"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
+        """returns a dictionary containing all keys/values of the instance"""
         dict_representation = self.__dict__.copy()
         dict_representation["__class__"] = self.__class__.__name__
         dict_representation["created_at"] = self.created_at.isoformat()
         dict_representation["updated_at"] = self.updated_at.isoformat()
         return dict_representation
+    
+    def delete(self):
+        """delete the current instance from the storage"""
+        models.storage.delete(self)
